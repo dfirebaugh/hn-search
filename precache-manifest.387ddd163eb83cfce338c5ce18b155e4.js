a7dccbc9ef3c3919b28a0a3d25bd818ea82f0364
Dustin Firebaugh <dafirebaugh@gmail.com>
2019-02-19 01:42:07 -0500

    Updates

---
+++ precache-manifest.387ddd163eb83cfce338c5ce18b155e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hn-search/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1434c5564e5ca7b88a34",
+    "url": "/hn-search/static/js/runtime~main.1434c556.js"
+  },
+  {
+    "revision": "dd2af1d9a18597ae3b7b",
+    "url": "/hn-search/static/js/main.dd2af1d9.chunk.js"
+  },
+  {
+    "revision": "fe2379ed7cb4da1885ac",
+    "url": "/hn-search/static/js/2.fe2379ed.chunk.js"
+  },
+  {
+    "revision": "dd2af1d9a18597ae3b7b",
+    "url": "/hn-search/static/css/main.0a22e13e.chunk.css"
+  },
+  {
+    "revision": "02ee0d67cbcca077a6252dc160e07995",
+    "url": "/hn-search/index.html"
+  }
+];
