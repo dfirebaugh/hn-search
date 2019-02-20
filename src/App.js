043f46942a38092c75c3c544310677859e2624a4
Dustin Firebaugh <dafirebaugh@gmail.com>
2019-02-19 20:56:59 -0500

    removing uneccesary Provider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import store from './store/index';
 import PreviousList from './components/PreviousList';
 import Search from './components/Search';
 import './App.css';
 
 const App = () => (
   <div className="App">
-    <Provider store={store}>
-      <PreviousList />
-      <Search />
-    </Provider>
+    <PreviousList />
+    <Search />
   </div>
 )
 
